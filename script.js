8d1a13d5fee4a4f9413644b463cc1613cc961fb6
Nickolay <anakolich@gmail.com>
2023-06-26 15:19:04 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+/*let number = 5;
+con leftBorderWidth = 1;
+number = 10;
+console.log(number);
+
+alert("Danger!");*/
+
+const category = "toys";
+
+console.log(`htpps://someurl.com/${category}/5`);
+
+
