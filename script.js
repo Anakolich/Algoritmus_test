f534f944d1161899b53be7c572ba45ad0193bbeb
Nickolay <anakolich@gmail.com>
2023-06-27 14:30:32 +0800

    unit19

--- script.js
+++ script.js
@@ -6,8 +6,27 @@ console.log(number);
 
 alert("Danger!");*/
 
-const category = "toys";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
 
-console.log(`htpps://someurl.com/${category}/5`);
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const answer = prompt("Один из просмотренных последних фильмов?",""),
+      reiting = +prompt("На сколько оцените его?",""),
+      answer_2 = prompt("Один из просмотренных последних фильмов?",""),
+      reiting_2 = +prompt("На сколько оцените его?","");
+
+
+personalMovieDB.movies[answer] = reiting;
+personalMovieDB.movies[answer_2] = reiting_2;
+
+console.log(personalMovieDB);
 
 
