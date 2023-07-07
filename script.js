8d1af86df1a92316da4f0791bfdb4b707e1d445a
Nickolay <anakolich@gmail.com>
2023-07-07 15:36:57 +0800

    test29

--- script.js
+++ script.js
@@ -6,27 +6,20 @@ console.log(number);
 
 alert("Danger!");*/
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
 
-console.log(numberOfFilms);
+    for (let i = 0; i < arr.length; i++) {
+         result[i] = arr[i];
+    }
+    // Пишем решение вот тут
+    
+    
+    // Не трогаем
+    console.log(result);
+    return result;
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
-
-const answer = prompt("Один из просмотренных последних фильмов?",""),
-      reiting = +prompt("На сколько оцените его?",""),
-      answer_2 = prompt("Один из просмотренных последних фильмов?",""),
-      reiting_2 = +prompt("На сколько оцените его?","");
-
-
-personalMovieDB.movies[answer] = reiting;
-personalMovieDB.movies[answer_2] = reiting_2;
-
-console.log(personalMovieDB);
-
-
+firstTask();
